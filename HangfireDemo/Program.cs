4de8eba89f7414b515715af2f4c75db66d7a31b8
Ahmed Abd-Elsalam <ahmed.abdelsalam@advaeg.com>
2024-03-30 14:17:28 +0200

    fix ConnectionString issue

--- HangfireDemo/Program.cs
+++ HangfireDemo/Program.cs
@@ -4,10 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString(connectionString)));
-builder.Services.AddHangfireServer();
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection"))); builder.Services.AddHangfireServer();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
