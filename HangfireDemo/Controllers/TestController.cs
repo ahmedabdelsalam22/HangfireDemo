10746d235f78779debc0a0da718365a3716e3bcb
Ahmed Abd-Elsalam <ahmed.abdelsalam@advaeg.com>
2024-03-30 14:29:25 +0200

    execute bk job imediately

---
+++ HangfireDemo/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangfireDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public void SendMSG(string name) 
+        {
+            Console.WriteLine("test send "+ name);
+        }
+
+        [HttpGet("test_bk_job")]
+        public ActionResult TestBackgroundJob()
+        {
+            BackgroundJob.Enqueue(()=> SendMSG("ahmed abd elsalam"));
+
+            return Ok();    
+        }
+    }
+}
