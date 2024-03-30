7da3b40919c68e0be50b193e79ed43d4ec15b923
Ahmed Abd-Elsalam <ahmed.abdelsalam@advaeg.com>
2024-03-30 14:45:10 +0200

    Test Recuiring Background Job

--- HangfireDemo/Controllers/TestController.cs
+++ HangfireDemo/Controllers/TestController.cs
@@ -14,19 +14,26 @@ namespace HangfireDemo.Controllers
             Console.WriteLine("test send "+ name);
         }
 
-        [HttpGet("test_bk_job")]
+        [HttpGet("test_Imdeiately_bk_job")]
         public ActionResult TestImdeiatelyBackgroundJob()
         {
             BackgroundJob.Enqueue(()=> SendMSG("ahmed abd elsalam"));
 
             return Ok();    
         }
-        [HttpGet("test_bk_job2")]
+        [HttpGet("test_Schedule_bk_job2")]
         public ActionResult TestScheduleBackgroundJob()
         {
             var time = DateTime.Now;
             Console.WriteLine(time);
             BackgroundJob.Schedule(() => SendMSG("ahmed abd elsalam") , TimeSpan.FromMinutes(1));
+
+            return Ok();
+        }
+        [HttpGet("test_Recuiring_bk_job3")]
+        public ActionResult TestRecuiringBackgroundJob()
+        {
+            RecurringJob.AddOrUpdate(() => SendMSG("ahmed abd elsalam"), Cron.Minutely);
 
             return Ok();
         }
